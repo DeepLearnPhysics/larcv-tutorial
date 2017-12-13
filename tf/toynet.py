5c89595dc57be328b2e8a03116bd0dd30398b837
drinkingkazu <kazuhiro@nevis.columbia.edu>
2017-12-13 15:04:21 -0500

    training classification tutorial

--- tf/toynet.py
+++ tf/toynet.py
@@ -9,43 +9,50 @@ def build(input_tensor, num_class=4, trainable=True, debug=True):
     if debug: print 'input tensor:', input_tensor.shape
 
     filters = 32
-
+    num_modules = 5
     with tf.variable_scope('conv'):
         for step in xrange(5):
+            stride = 2
+            if step: stride = 1
+            net = slim.conv2d(inputs        = net,        # input tensor
+                              num_outputs   = filters,    # number of filters (neurons) = # of output feature maps
+                              kernel_size   = [3,3],      # kernel size
+                              stride        = stride,     # stride size
+                              trainable     = trainable,  # train or inference
+                              activation_fn = tf.nn.relu, # relu
+                              scope         = 'conv%da_conv' % step)
 
-            net = slim.conv2d(inputs      = net,       # input tensor
-                              num_outputs = filters,   # number of filters (neurons) = # of output feature maps
-                              kernel_size = [3,3],     # kernel size
-                              stride      = 1,         # stride size
-                              trainable   = trainable, # train or inference
-                              scope       = 'conv%d_conv' % step)
-            
-            net = slim.max_pool2d(inputs      = net,    # input tensor
-                                  kernel_size = [3,3],  # kernel size
-                                  stride      = 2,      # stride size
-                                  scope       = 'conv%d_pool' % step)
-            
+            net = slim.conv2d(inputs        = net,        # input tensor
+                              num_outputs   = filters,    # number of filters (neurons) = # of output feature maps
+                              kernel_size   = [3,3],      # kernel size
+                              stride        = 1,          # stride size
+                              trainable     = trainable,  # train or inference
+                              activation_fn = tf.nn.relu, # relu
+                              scope         = 'conv%db_conv' % step)
+            if (step+1) < num_modules:
+                net = slim.max_pool2d(inputs      = net,    # input tensor
+                                      kernel_size = [2,2],  # kernel size
+                                      stride      = 2,      # stride size
+                                      scope       = 'conv%d_pool' % step)
+
+            else:
+                net = tf.layers.average_pooling2d(inputs = net,
+                                                  pool_size = [net.get_shape()[-2].value,net.get_shape()[-3].value],
+                                                  strides = 1,
+                                                  padding = 'valid',
+                                                  name = 'conv%d_pool' % step)
             filters *= 2
 
             if debug: print 'After step',step,'shape',net.shape
 
-    with tf.variable_scope('fc'):
+    with tf.variable_scope('final'):
         net = slim.flatten(net, scope='flatten')
+
         if debug: print 'After flattening', net.shape
-        
-        net = slim.fully_connected(net, 4096, scope='fc1')
-        if debug: print 'After fc1', net.shape
-        if trainable:
-            net = slim.dropout(net, keep_prob=0.5, is_training=trainable, scope='fc1_dropout')
-            
-        net = slim.fully_connected(net, 4096, scope='fc2')
-        if debug: print 'After fc2', net.shape
-        if trainable:
-            net = slim.dropout(net, keep_prob=0.5, is_training=trainable, scope='fc2_dropout')
-            
-        net = slim.fully_connected(net, int(num_class), scope='fc_final')
-            
-        if debug: print 'After fc_final', net.shape
+
+        net = slim.fully_connected(net, int(num_class), scope='final_fc')
+
+        if debug: print 'After final_fc', net.shape
 
     return net
 
--- tf/train_pid.py
+++ tf/train_pid.py
@@ -1,17 +1,30 @@
 from larcv import larcv
 from larcv.dataloader2 import larcv_threadio
 import toynet
+#import resnet as toynet
 import numpy as np
 import tensorflow as tf
 import os,sys,time
-
-# constants
-IO_CONFIG  = 'io.cfg'
-BATCH_SIZE = 50
-LOGDIR     = 'log'
-ITERATIONS = 1000
-SAVE_SUMMARY = 10
+#
+# Configurations
+#
+TRAIN_IO_CONFIG  = 'io_train.cfg'
+TEST_IO_CONFIG   = 'io_test.cfg'
+TRAIN_BATCH_SIZE = 50
+TEST_BATCH_SIZE  = 100
+LOGDIR = 'log'
+ITERATIONS = 10000
+SAVE_SUMMARY = 20
 SAVE_WEIGHTS = 100
+
+# Check log directory is empty
+train_logdir = os.path.join(LOGDIR,'train')
+test_logdir  = os.path.join(LOGDIR,'test')
+if not os.path.isdir(train_logdir): os.makedirs(train_logdir)
+if not os.path.isdir(test_logdir):  os.makedirs(test_logdir)
+if len(os.listdir(train_logdir)) or len(os.listdir(test_logdir)):
+  sys.stderr.write('Error: train or test log dir not empty...\n')
+  raise OSError
 
 #
 # Step 0: IO
@@ -19,14 +32,24 @@ SAVE_WEIGHTS = 100
 train_io = larcv_threadio()        # create io interface 
 train_io_cfg = {'filler_name' : 'TrainIO',
                 'verbosity'   : 0, 
-                'filler_cfg'  : IO_CONFIG}
+                'filler_cfg'  : TRAIN_IO_CONFIG}
 train_io.configure(train_io_cfg)   # configure
-train_io.start_manager(BATCH_SIZE) # start read thread
+train_io.start_manager(TRAIN_BATCH_SIZE) # start read thread
 time.sleep(2)
 # retrieve data dimensions to define network later
 train_io.next()
-dim_data  = train_io.fetch_data('image').dim()
-dim_label = train_io.fetch_data('label').dim()
+dim_data  = train_io.fetch_data('train_image').dim()
+dim_label = train_io.fetch_data('train_label').dim()
+
+test_io = larcv_threadio()        # create io interface
+test_io_cfg = {'filler_name' : 'TestIO',
+               'verbosity'   : 0,
+               'filler_cfg'  : TEST_IO_CONFIG}
+test_io.configure(test_io_cfg)   # configure
+test_io.start_manager(TEST_BATCH_SIZE) # start read thread
+time.sleep(2)
+# retrieve data dimensions to define network later
+test_io.next()
 
 #
 # Step 1: Network
@@ -37,7 +60,7 @@ data_tensor_2d = tf.reshape(data_tensor, [-1,dim_data[1],dim_data[2],dim_data[3]
 # Let's keep 10 random set of images in the log
 tf.summary.image('input',data_tensor_2d,10)
 # build net
-net = toynet.build(input_tensor=data_tensor_2d, num_class=dim_label[1], trainable=True, debug=False)
+net = toynet.build(input_tensor=data_tensor_2d, num_class=dim_label[1], trainable=True, debug=True)
 # Define accuracy
 with tf.name_scope('accuracy'):
   correct_prediction = tf.equal(tf.argmax(net,1), tf.argmax(label_tensor,1))
@@ -47,7 +70,8 @@ with tf.name_scope('accuracy'):
 with tf.name_scope('train'):
   cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_tensor, logits=net))
   tf.summary.scalar('cross_entropy',cross_entropy)
-  train_step = tf.train.RMSPropOptimizer(0.0003).minimize(cross_entropy)  
+  train_step = tf.train.RMSPropOptimizer(0.0001).minimize(cross_entropy)  
+  #train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)  
 
 #
 # Step 2: weight saver & summary writer
@@ -59,8 +83,10 @@ sess = tf.InteractiveSession()
 # Initialize variables
 sess.run(tf.global_variables_initializer())
 # Create a summary writer handle
-writer=tf.summary.FileWriter(LOGDIR)
-writer.add_graph(sess.graph)
+writer_train=tf.summary.FileWriter(train_logdir)
+writer_train.add_graph(sess.graph)
+writer_test=tf.summary.FileWriter(test_logdir)
+writer_test.add_graph(sess.graph)
 # Create weights saver
 saver = tf.train.Saver()
 
@@ -69,22 +95,35 @@ saver = tf.train.Saver()
 #
 for i in range(ITERATIONS):
 
-  batch_data  = train_io.fetch_data('image').data()
-  batch_label = train_io.fetch_data('label').data()
+  train_data  = train_io.fetch_data('train_image').data()
+  train_label = train_io.fetch_data('train_label').data()
 
-  feed_dict = { data_tensor  : batch_data,
-                label_tensor : batch_label }
-
-  train_io.next()
+  feed_dict = { data_tensor  : train_data,
+                label_tensor : train_label }
 
   loss, acc, _ = sess.run([cross_entropy, accuracy, train_step], feed_dict=feed_dict)
-
-  sys.stdout.write('Training in progress @ step %d loss %g accuracy %g          \r' % (i,loss,acc))
+  sys.stdout.write('Training in progress @ step %d loss %g accuracy %g          \n' % (i,loss,acc))
   sys.stdout.flush()
 
   if (i+1)%SAVE_SUMMARY == 0:
-    s = sess.run(merged_summary, feed_dict=feed_dict)
-    writer.add_summary(s,i)
+    # Save train log
+    #s = sess.run(merged_summary, feed_dict=feed_dict)
+    #writer_train.add_summary(s,i)
+
+    # Calculate & save test log
+    test_data  = test_io.fetch_data('test_image').data()
+    test_label = test_io.fetch_data('test_label').data()
+    feed_dict  = { data_tensor  : test_data,
+                   label_tensor : test_label }
+    loss, acc = sess.run([cross_entropy, accuracy], feed_dict=feed_dict)
+    sys.stdout.write('Testing in progress @ step %d loss %g accuracy %g          \n' % (i,loss,acc))
+    sys.stdout.flush()
+    #s = sess.run(merged_summary, feed_dict=feed_dict)
+    #writer_test.add_summary(s,i)
+
+    test_io.next()
+
+  train_io.next()
 
   if (i+1)%SAVE_WEIGHTS == 0:
     ssf_path = saver.save(sess,'weights/toynet',global_step=i)
